f887c3d9b4eba700a4c73a1487468c5c083f8fad
ChristianPons <82019335+ChristianPons@users.noreply.github.com>
2021-05-18 18:47:52 +0200

    Controles añadidos

--- tetrisprojectfx/src/main/java/tetrisprojectfxgroup/tetrisprojectfx/controllers/LoginController.java
+++ tetrisprojectfx/src/main/java/tetrisprojectfxgroup/tetrisprojectfx/controllers/LoginController.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import model.BasicData;
