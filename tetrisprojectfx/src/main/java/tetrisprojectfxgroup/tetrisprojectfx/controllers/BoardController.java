6fd3778f9dd2c2b384547b62977ebad311cf3cb7
Mig27380 <Mig27380@gmail.com>
2021-05-19 21:48:00 +0200

    Continued with BoardController

--- tetrisprojectfx/src/main/java/tetrisprojectfxgroup/tetrisprojectfx/controllers/BoardController.java
+++ tetrisprojectfx/src/main/java/tetrisprojectfxgroup/tetrisprojectfx/controllers/BoardController.java
@@ -5,8 +5,13 @@ package tetrisprojectfxgroup.tetrisprojectfx.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+
 import gamecore.logic.Board;
 import gamecore.logic.GameLogic;
+import gamecore.logic.Piece;
+import gamecore.logic.Tile;
 import gamecore.resources.VariableTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -18,19 +23,12 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import model.BasicData;
 
 
 public class BoardController extends GameLogic implements Initializable{
 	
-	public BoardController() {
-		super(1, 3);
+	boolean isSoftDropping = false;
 
-	}
-
-
-
-	BasicData data;
 	@FXML Label level;
 	@FXML Label score;
 	@FXML GridPane piece1;
@@ -39,20 +37,36 @@ public class BoardController extends GameLogic implements Initializable{
 	@FXML GridPane pieceHolder;
 	@FXML GridPane board;
 	Pane[][] panelBoard = new Pane[Board.BOARD_HEIGHT][Board.BOARD_WIDTH];
-	Color[] color = {Color.TRANSPARENT, Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.GREEN, Color.RED, Color.BLUE, Color.ORANGE};
-	VariableTimer timer = new VariableTimer() {
-
+	Color[] colors = {Color.TRANSPARENT, Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.GREEN, Color.RED, Color.BLUE, Color.ORANGE};
+	VariableTimer timer = new VariableTimer(SPEED_CURVE[getLevel()]) {
 		@Override
 		public void task() {
-			moveDown(false);
-			
+			moveDown(isSoftDropping);
 		}
 		
 	};
 	
+	public BoardController() {
+		super(1, 3);
+	}
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setGridPane();
+		try {
+			GlobalScreen.registerNativeHook();
+		} catch (NativeHookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	@Override
 	public void levelUp() {
 		super.levelUp();
+		timer.setDelayedInterval(SPEED_CURVE[getLevel()]);
 	}
 	
 	@Override
@@ -61,11 +75,24 @@ public class BoardController extends GameLogic implements Initializable{
 	}
 	
 	public void setControls() {
-
+		
 	}
 	
+	@Override
 	public void refresh() {
 		int[][]board = getBoard().getBoard();
+		Piece piece = getCurrentPiece();
+		for(Tile tile : piece.getTiles()) {
+			board[piece.getTileAbsoluteY(tile)][piece.getTileAbsoluteX(tile)] = tile.getValue();
+		}
+		if(isSoftDropping) board[0][0] = 1;
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length; j++) {
+				panelBoard[Board.BOARD_HEIGHT - i - 3][j].setBackground(new Background(new BackgroundFill(colors[board[i][j]], CornerRadii.EMPTY, Insets.EMPTY)));
+			}
+		}
+		
+		System.out.println(getBoard().getJSON());
 	}
 	
 	public void setGridPane() {
@@ -78,11 +105,7 @@ public class BoardController extends GameLogic implements Initializable{
 		}
 	}
 	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		setGridPane();
-		panelBoard[5][5].setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-	}
+	
 	
 	
 }
